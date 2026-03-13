a50e9c50711051c3cce66917184da97faf1f1dca
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-13 22:54:57 +0100

    Create src/wallet/connect.ts

---
+++ src/wallet/connect.ts
@@ -0,0 +1,16 @@
+import { AppConfig, UserSession, showConnect } from "@stacks/connect"
+
+const appConfig = new AppConfig(["store_write"])
+
+export const userSession = new UserSession({ appConfig })
+
+export function connectWallet() {
+  showConnect({
+    userSession,
+    appDetails: {
+      name: "STX Vault",
+      icon: "/logo.png"
+    },
+    onFinish: () => window.location.reload()
+  })
+}
