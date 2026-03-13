e0207c79b9b1b841a7e0215821f6c1fd92f685a9
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-13 22:58:29 +0100

    Create examples/react-demo/app.js

---
+++ examples/react-demo/app.js
@@ -0,0 +1,15 @@
+import { connectWallet, lockSTX } from "stx-vault-sdk"
+
+async function start() {
+  await connectWallet()
+
+  await lockSTX(
+    100000000,
+    900000,
+    PRIVATE_KEY,
+    CONTRACT_ADDRESS,
+    "stx-vault"
+  )
+}
+
+start()
