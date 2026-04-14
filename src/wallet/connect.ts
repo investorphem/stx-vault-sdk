2c61747bc08548b74bdf0b08039c2c1ae5e14b1c
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-14 09:11:29 +0100

    Update connect.ts

--- src/wallet/connect.ts
+++ src/wallet/connect.ts
@@ -6,7 +6,7 @@ export const userSession = new UserSession({ appConfig })
 
 export function connectWallet() {
   showConnect(
-    userSe
+    userS
     appDetails
       name: "STXVaut",
       icon: "/log.png"
