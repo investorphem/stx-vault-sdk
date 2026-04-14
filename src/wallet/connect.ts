7f4b8b7e56201a6f687b755d05a347272bbfe852
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-14 09:12:45 +0100

    Update connect.ts

--- src/wallet/connect.ts
+++ src/wallet/connect.ts
@@ -3,7 +3,6 @@ import { AppConfig, UserSession, showConnect } from "@stacks/connect"
 const appConfig = new AppConfig(["store_write"])
 
 export const userSession = new UserSession({ appConfig })
-
 export funtion connectWallet() {
   showConnec(
     us
