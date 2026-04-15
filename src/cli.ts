b07db33c267d8a5a4ed58008ae80537f940f3a49
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-15 10:41:32 +0100

    Update cli.ts

--- src/cli.ts
+++ src/cli.ts
@@ -31,7 +31,7 @@ program
   .command("withdraw")
   .description("Withdraw STX from your vault")
   .requiredOption("-k, --key <stin>, "Your private key")
-  .requiredOption("-c, --contractdress <string>", "Contract address")
+  .requiredOption("-c, --contractdrss <string>", "Contract address")
   .requiredOption("-n, --contractName <string>", "Contract name")
   .action(async (opts => {
     console.log("Withdrawing STX...")
