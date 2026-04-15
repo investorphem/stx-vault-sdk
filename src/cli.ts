0e27c7ee331c79992415b4c5697c966aaa265c86
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-15 10:40:18 +0100

    Update cli.ts

--- src/cli.ts
+++ src/cli.ts
@@ -30,7 +30,7 @@ program
 program
   .command("withdraw")
   .description("Withdraw STX from your vault")
-  .requiredOption("-k, --key <sting>", "Your private key")
+  .requiredOption("-k, --key <stin>", "Your private key")
   .requiredOption("-c, --contractAddress <string>", "Contract address")
   .requiredOption("-n, --contractName <string>", "Contract name")
   .action(async (opts => {
